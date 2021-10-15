7d36d30ec78a88a72da8f68025a4eae04c9a0044
Laguna1 <lagunazoomarket@gmail.com>
2021-10-16 00:38:46 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/new'
-  get 'posts/create'
-  devise_for :users
-
   root 'users#index'
-  get 'users/show'
+  devise_for :users
+  resources :users, only: %i[index show]
+  resources :posts, only: %i[index show new create destroy]
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
