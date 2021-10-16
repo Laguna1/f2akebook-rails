0b35dd80d07c421d1e8d22189bbad304e76c3c22
Laguna1 <lagunazoomarket@gmail.com>
2021-10-16 22:18:10 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'comments/new'
   get 'comments/create'
-  get 'comments/destroy'
   root 'users#index'
   devise_for :users
   resources :users, only: %i[index show]
   resources :posts, only: %i[index show new create destroy]
+  resources :comments, only: %i[new create destroy]
 end
