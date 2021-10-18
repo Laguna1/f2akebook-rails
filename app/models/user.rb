67bdc8487d270eba2544d7fd9d18c9b24226a718
Laguna1 <lagunazoomarket@gmail.com>
2021-10-19 01:00:43 +0300

    Add user model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :fname, presence: true, length: { in: 3..15 }
+  validates :lname, presence: true, length: { in: 3..15 }
 end
