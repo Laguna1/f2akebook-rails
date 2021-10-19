5ce3b36d56e010ebc298f9d121d58323d4cb8197
Laguna1 <lagunazoomarket@gmail.com>
2021-10-20 01:37:11 +0300

    Update routes to accept &decline friendships

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   root 'users#index'
   devise_for :users
   resources :users, only: %i[index show] do
-    resources :friendships, only: :create
+    resources :friendships, only: %i[create] do
+      collection do
+        get 'accept_friend'
+        get 'decline_friend'
+      end
+    end
   end
   resources :posts, only: %i[index show new create destroy] do
     resources :likes, only: %i[create]
